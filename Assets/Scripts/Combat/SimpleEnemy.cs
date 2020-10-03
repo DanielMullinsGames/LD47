4edd0b7b4e0a36d61d1545a23e0ab9fd86a65f70
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-10-03 12:34:22 -0700

    - progress on enemy combat event

---
+++ Assets/Scripts/Combat/SimpleEnemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : MonoBehaviour
+{
+    public bool ReachedTarget { get; private set; }
+
+    [SerializeField]
+    private Animator anim;
+
+    [SerializeField]
+    private float speed;
+
+    private bool hasTarget;
+    private float targetX;
+
+    public bool Dead { get; private set; }
+
+	public void ResetToStart()
+    {
+        anim.Play("running", 0, 0f);
+        hasTarget = false;
+    }
+
+    public void ResetToEnd()
+    {
+        anim.Play("die", 0, 1f);
+    }
+
+    public void Die()
+    {
+        Dead = true;
+    }
+
+    public void MoveToTarget(float xPos)
+    {
+        targetX = xPos;
+        hasTarget = true;
+        ReachedTarget = false;
+    }
+
+    private void Update()
+    {
+        if (!Dead && hasTarget)
+        {
+            UpdateMoveToTarget();
+        }
+    }
+
+    private void UpdateMoveToTarget()
+    {
+        if (targetX < transform.position.x)
+        {
+            transform.position += new Vector3(Time.deltaTime * speed, 0f, 0f);
+
+            if (transform.position.x < targetX)
+            {
+                ReachedTarget = true;
+                transform.position = new Vector2(targetX, transform.position.y);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Timeline/SpecificEvent/EnemyEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEvent : TimelineEvent
+{
+    [SerializeField]
+    private SimpleEnemy enemy;
+
+    [SerializeField]
+    private Transform startMarker;
+
+    [SerializeField]
+    private Transform endMarker;
+
+    protected override void ResetToStart()
+    {
+        enemy.transform.position = new Vector2(startMarker.position.x, enemy.transform.position.y);
+        enemy.ResetToStart();
+    }
+
+    protected override void ResetToEnd()
+    {
+        enemy.transform.position = new Vector2(endMarker.position.x, enemy.transform.position.y);
+        enemy.ResetToEnd();
+    }
+
+    protected override IEnumerator EventSequence()
+    {
+        enemy.MoveToTarget(endMarker.position.x);
+        yield return new WaitUntil(() => enemy.Dead || enemy.ReachedTarget);
+
+        if (!enemy.Dead)
+        {
+            // raise weapon
+            // wait to see if dead
+            // if lived, kill player + reset
+        }
+    }
+}
