2ad77e335f4363085e2f2984ebffdae7302c4064
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-10-03 12:13:18 -0700

    - strawberry enemy animation

---
+++ Assets/Scripts/Base/Animation/AnimMethods.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimMethods : MonoBehaviour
+{
+
+    public bool AudioMuted { get { return audioMuted; } set { audioMuted = value; } }
+    private bool audioMuted;
+
+    void SendMessageUp(string message)
+    {
+        gameObject.SendMessageUpwards(message, SendMessageOptions.DontRequireReceiver);
+    }
+
+    void Destroy()
+    {
+        Destroy(transform.parent.gameObject);
+    }
+
+    void PlaySound(string soundId)
+    {
+
+    }
+}
