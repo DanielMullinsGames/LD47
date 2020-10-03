2e5b96d8488a87733a5cda3df79467efe9e93c88
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-10-02 20:11:46 -0700

    - scrolling dust - player prefab + idle animation

---
+++ Assets/Scripts/Environment/UVScroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScroll : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    float currentScroll = 0f;
+
+    private void Update()
+    {
+        currentScroll += speed * Time.deltaTime;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(currentScroll, 0);
+    }
+}
