a9c7594e99ce581435b59e239b28112ab12f90c3
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-10-03 21:46:04 -0700

    - weapon rack event at start

---
+++ Assets/Scripts/Timeline/SpecificEvent/WeaponsRackEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsRackEvent : TimelineEvent
+{
+    [SerializeField]
+    private SpriteRenderer weapon;
+
+    protected override void ResetToStart()
+    {
+        weapon.enabled = true;
+    }
+
+    protected override void ResetToEnd()
+    {
+        weapon.enabled = false;
+    }
+
+    protected override IEnumerator EventSequence()
+    {
+        yield return new WaitForSeconds(0.25f);
+        // cute animation
+        yield return new WaitForSeconds(0.5f);
+    }
+}
