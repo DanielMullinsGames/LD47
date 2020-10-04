97f7ed69bac283f7368289638bb3e6363d3d2553
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-10-04 11:24:14 -0700

    - completed starting events

--- Assets/Scripts/Timeline/SpecificEvent/DialogueEvent.cs
+++ Assets/Scripts/Timeline/SpecificEvent/DialogueEvent.cs
@@ -1,11 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pixelplacement;
 
 public class DialogueEvent : TimelineEvent
 {
     [SerializeField]
     private List<DialogueBubble> dialogueBubbles;
+
+    [SerializeField]
+    private Transform liveComrade;
+    private Vector2 comradeStartPos;
+
+    private void Awake()
+    {
+        if (liveComrade != null)
+        {
+            comradeStartPos = liveComrade.position;
+        }
+    }
+
+    protected override void ResetToStart()
+    {
+        if (liveComrade != null)
+        {
+            liveComrade.position = comradeStartPos;
+            liveComrade.gameObject.SetActive(true);
+            liveComrade.GetComponentInChildren<Animator>().Play("idle", 0, 0f);
+        }
+    }
+
+    protected override void ResetToEnd()
+    {
+        if (liveComrade != null)
+        {
+            liveComrade.position = comradeStartPos;
+            liveComrade.gameObject.SetActive(false);
+        }
+    }
 
     protected override IEnumerator EventSequence()
     {
@@ -17,6 +49,14 @@ public class DialogueEvent : TimelineEvent
             yield return b.PlayDialogue();
             yield return new WaitForSeconds(0.1f);
             b.gameObject.SetActive(false);
+        }
+
+        if (liveComrade != null)
+        {
+            liveComrade.GetComponentInChildren<Animator>().SetTrigger("slide");
+            Tween.Position(liveComrade, liveComrade.transform.position + (Vector3.right * 12f), 0.5f, 0f, Tween.EaseInOut);
+            yield return new WaitForSeconds(0.5f);
+            liveComrade.gameObject.SetActive(false);
         }
 
         PlayerController.Instance.enabled = true;
