668481ffc8f5444784a79e0c17e26858647a5e4a
Daniel (Home PC) <daniel.mullins.games@gmail.com>
2020-10-04 08:25:00 -0700

    - fallen comrade animation + dialogue

--- Assets/Scripts/Timeline/SpecificEvent/DialogueEvent.cs
+++ Assets/Scripts/Timeline/SpecificEvent/DialogueEvent.cs
@@ -10,7 +10,7 @@ public class DialogueEvent : TimelineEvent
     protected override IEnumerator EventSequence()
     {
         PlayerController.Instance.enabled = false;
-
+        yield return new WaitForSeconds(0.2f);
         foreach (DialogueBubble b in dialogueBubbles)
         {
             b.gameObject.SetActive(true);
